10584ecbd9dd08ed2eb7308c72b265fd26c17e22
BuildTools <unconfigured@null.spigotmc.org>
2020-07-03 17:11:13 -0600

    added test sprite and started movement script

---
+++ GameDevelopment/VaccineGame/Assets/MovementScript1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool up = Input.GetButton("up" | "w");
+        bool down = Input.GetButton("down" | "s");
+        bool left = Input.GetButton("left" | "a");
+        bool right = Input.GetButton("right" | "d");
+        
+    }
+}
